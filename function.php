c15654fc7960b3cc2d3c2b9617a7953023b07890
⭐Nathan ⭐ <59396862+diggyhard@users.noreply.github.com>
2021-08-11 21:33:59 +0200

    Update function.php

--- function.php
+++ function.php
@@ -2,24 +2,29 @@
 function mdp($mdp, $action = 'illisible')
 {
 
-    $illisible = "AES-256-CBC";
-
-    $clef2 = '8hgn5HJ5g27';
+    
+    
+    
+    
+    $clef = 'BC74EGDD4GMPV047515BD7Y65912'; // you can define your unique encryption key, change it !
+    
+    
+    
+  
 
     $hash = hash('sha256', $clef);
+    
+    $iv = substr(hash('sha256', '8hgn5HJ5g27') , 0, 16);
 
-    $iv = substr(hash('sha256', $clef2) , 0, 16);
-
-    if ($action == 'illisible')
-    { // Encrypter
-        $reponse = openssl_encrypt($mdp, $illisible, $hash, 0, $iv);
+    if ($action == 'illisible') { 
+    
+        $reponse = openssl_encrypt($mdp, 'AES-256-CBC', $hash, 0, $iv);
 
         $reponse = base64_encode($reponse);
 
-    }
-    else if ($action == 'lisible')
-    { // Decrypter
-        $reponse = openssl_decrypt(base64_decode($mdp) , $illisible, $hash, 0, $iv);
+    } else if ($action == 'lisible') {
+    
+        $reponse = openssl_decrypt(base64_decode($mdp) , 'AES-256-CBC', $hash, 0, $iv);
 
     }
 
